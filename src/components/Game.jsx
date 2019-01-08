55e5d16309ca80a2ac1dd5a2c5517472384aa0b0
prajwaldhawarikar1997 <prajwal.dhawarikar@gmail.com>
2019-01-08 17:12:16 +0530

    changed name

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,7 +12,7 @@ class Game extends Component {
     render() { 
         return ( 
             <div className="container">
-                <h3>Play Nine</h3>
+                <h3>Play Nine Demo</h3>
                 <hr/>
                 <div className="row">
                     <Stars />
